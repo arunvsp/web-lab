07b9ec7af1856a37fabda3e2eec5c048ac5c53c8
arunvsp <45871297+arunvsp@users.noreply.github.com>
2018-12-14 14:52:01 +0530

    Add files via upload

---
+++ sender1.java
@@ -0,0 +1,13 @@
+import java.net.*;
+public class sender1
+{
+	public static void  main(String[] args) throws Exception
+	{
+	
+		InetAddress ip=InetAddress.getByName("www.google.com");
+		System.out.println(ip);
+		
+			}
+	
+	
+}
